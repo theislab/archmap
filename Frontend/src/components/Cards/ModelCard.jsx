fdb3068083d6554166022fd27ef7facd2036d69a
rskorobogat <rskorobogat@gmail.com>
2022-10-19 11:05:19 +0200

    start of card refactor. Remove unnecessary div

--- Frontend/src/components/Cards/ModelCard.jsx
+++ Frontend/src/components/Cards/ModelCard.jsx
@@ -47,105 +47,100 @@ export const ModelCard = ({ width = "100%", height = "100%", title, description,
 
   return (
     <Box
+      ref={ref}
       sx={{
         width: width,
-        height: height
+        height: height,
+        position: "relative",
+        cursor: disabled ? "default" : "pointer",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        backgroundColor: 'yellow'
       }}
+      onMouseEnter={() => setHover(true)}
+      onMouseLeave={() => setHover(false)}
     >
-      <Box
-        ref={ref}
-        sx={{
-          width: "100%",
-          height: "100%",
-          position: "relative",
-          cursor: disabled ? "default": "pointer",
-          display: "flex",
-          flexDirection: "column",
-          justifyContent: "center"
-        }}
-        onMouseEnter={() => setHover(true)}
-        onMouseLeave={() => setHover(false)}
-      >
-        {
-          !disabled && hover &&
+      {
+        !disabled && hover &&
+        <Box
+          style={{ background: "linear-gradient(#4F83CC, #01579B)" }}
+          sx={{
+            position: "absolute",
+            width: "100%",
+            height: "100%",
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'center',
+            borderRadius: "1.2rem",
+            cursor: "pointer",
+            opacity: 0.95,
+            boxShadow: "0px 4px 6px 0px rgba(1, 87, 155, .20), 0px 0px 1px 0px rgba(1, 87, 155, .32)"
+          }}>
           <Box
-            style={{ background: "linear-gradient(#4F83CC, #01579B)" }}
             sx={{
-              position: "absolute",
-              width: "100%",
-              height: "100%",
+              margin: 'auto',
+              width: '70%',
+              height: 'auto',
               display: 'flex',
               flexDirection: 'column',
-              justifyContent: 'center',
-              borderRadius: "1.2rem",
-              cursor: "pointer",
-              opacity: 0.95,
-              boxShadow: "0px 4px 6px 0px rgba(1, 87, 155, .20), 0px 0px 1px 0px rgba(1, 87, 155, .32)"
-            }}>
-            <Box
-              sx={{
-                margin: 'auto',
-                width: '70%',
-                height: 'auto',
-                display: 'flex',
-                flexDirection: 'column',
-                justifyContent: 'space-evenly',
-                gap: '5px',
-              }}
-            >
-              { !isSearchPage && <OutlinedButton content="Select" onClick={onSelect} /> }
-              <OutlinedButton content="Learn More" link={learnMoreLink} onClick={(e) => e.stopPropagation()}/>
-              {
-                disabled &&
-                <Typography sx={{ color: colors.primary[900], fontSize: "12px", textDecoration: 'underline', textAlign: "center" }}>
-                  Model unavailable for mapping
-                </Typography>
-              }
-            </Box>
-          </Box>
-        }
-        {/* DISABLED OVERLAY BOX */}
-        {
-          disabled &&
-          <Box
-            style={{ background: "linear-gradient(#e7e7e7, #d0d0d0)" }}
-            sx={{
-              position: "absolute",
-              width: "100%",
-              height: "100%",
-              display: 'flex',
-              flexDirection: 'column',
-              justifyContent: 'center',
-              borderRadius: "1.2rem",
-              cursor: "pointer",
-              opacity: 0.95,
-            }}>
+              justifyContent: 'space-evenly',
+              gap: '5px',
+            }}
+          >
+            {!isSearchPage && <OutlinedButton content="Select" onClick={onSelect} />}
+            <OutlinedButton content="Learn More" link={learnMoreLink} onClick={(e) => e.stopPropagation()} />
             {
-              !hover &&  
-              <Typography sx={{ position: "absolute", fontSize: "12px", fontWeight: "bold", color: colors.neutral[900], textAlign: "center", left: "28%" }}>
-                Not Compatible
+              disabled &&
+              <Typography sx={{ color: colors.primary[900], fontSize: "12px", textDecoration: 'underline', textAlign: "center" }}>
+                Model unavailable for mapping
               </Typography>
             }
-            <Box
-              sx={{
-                margin: 'auto',
-                width: '70%',
-                height: 'auto',
-                display: 'flex',
-                flexDirection: 'column',
-                justifyContent: 'space-evenly',
-                gap: '5px',
-              }}
-            >
-
-              { hover && <OutlinedButton content="Learn More" link={learnMoreLink} bg={colors.neutral[800]} color={colors.neutral[800]} onClick={(e) => e.stopPropagation()} bgHover={colors.neutral[100]} colorHover={colors.neutral[600]}/> }
-              {/* <CustomButton type="outlined" href={learnMoreLink ? `#${learnMoreLink}` : null} onClick={(e) => e.stopPropagation()}>Learn More</CustomButton> */}
-            </Box>
           </Box>
-        }
-        { 
-          !disabled &&
-          <Box sx={{
+        </Box>
+      }
+      {/* DISABLED OVERLAY BOX */}
+      {
+        disabled &&
+        <Box
+          style={{ background: "linear-gradient(#e7e7e7, #d0d0d0)" }}
+          sx={{
+            position: "absolute",
+            width: "100%",
+            height: "100%",
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'center',
+            borderRadius: "1.2rem",
+            cursor: "pointer",
+            opacity: 0.95,
+          }}>
+          {
+            !hover &&
+            <Typography sx={{ position: "absolute", fontSize: "12px", fontWeight: "bold", color: colors.neutral[900], textAlign: "center", left: "28%" }}>
+              Not Compatible
+            </Typography>
+          }
+          <Box
+            sx={{
+              margin: 'auto',
+              width: '70%',
+              height: 'auto',
+              display: 'flex',
+              flexDirection: 'column',
+              justifyContent: 'space-evenly',
+              gap: '5px',
+            }}
+          >
+
+            {hover && <OutlinedButton content="Learn More" link={learnMoreLink} bg={colors.neutral[800]} color={colors.neutral[800]} onClick={(e) => e.stopPropagation()} bgHover={colors.neutral[100]} colorHover={colors.neutral[600]} />}
+            {/* <CustomButton type="outlined" href={learnMoreLink ? `#${learnMoreLink}` : null} onClick={(e) => e.stopPropagation()}>Learn More</CustomButton> */}
+          </Box>
+        </Box>
+      }
+      {
+        !disabled &&
+        <Box sx={{
           width: '100%',
           height: "100%",
           display: "flex",
@@ -153,30 +148,29 @@ export const ModelCard = ({ width = "100%", height = "100%", title, description,
           p: "1.2rem",
           boxShadow: hover ? "none" : "0px 0px 10px rgba(0, 0, 0, 0.15)",
           borderRadius: "1.2rem",
-          borderStyle:"solid",
+          borderStyle: "solid",
           borderColor: selected ? "#008BF5" : 'transparent',
-          borderWidth:"4px",
+          borderWidth: "4px",
         }}
         >
           <Typography sx={{ fontSize: "1.4rem", fontWeight: "bold" }}>{`Model ${title}`}</Typography>
           <Typography sx={{ fontSize: "1rem", color: colors.neutral[800] }}>{description}</Typography>
         </Box>}
-        { 
-          disabled &&
-          <Box sx={{
-            width: '100%',
-            height: "100%",
-            display: "flex",
-            flexDirection: "column",
-            p: "1.2rem",
-            borderRadius: "1.2rem",
-          }}
-          >
-            <Typography sx={{ fontSize: "1.4rem", fontWeight: "bold" }}>{title}</Typography>
-            <Typography sx={{ fontSize: "1rem", color: colors.neutral[800] }}>{description}</Typography>
-          </Box>
-        }
-      </Box>
+      {
+        disabled &&
+        <Box sx={{
+          width: '100%',
+          height: "100%",
+          display: "flex",
+          flexDirection: "column",
+          p: "1.2rem",
+          borderRadius: "1.2rem",
+        }}
+        >
+          <Typography sx={{ fontSize: "1.4rem", fontWeight: "bold" }}>{title}</Typography>
+          <Typography sx={{ fontSize: "1rem", color: colors.neutral[800] }}>{description}</Typography>
+        </Box>
+      }
     </Box>
   )
 }
