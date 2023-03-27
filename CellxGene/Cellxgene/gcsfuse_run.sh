81bfbd60d2fe9f68cc33da3fe167855360195ebc
xaviergeorge <13365640+xaviergeorge@users.noreply.github.com>
2023-03-27 17:14:53 +0200

    mount bug fix

--- CellxGene/Cellxgene/gcsfuse_run.sh
+++ CellxGene/Cellxgene/gcsfuse_run.sh
@@ -1,6 +1,22 @@
 #!/bin/bash
+set -eo pipefail;
 
-gcloud auth activate-service-account ${SERVICE_ACCOUNT_EMAIL} --project=${PROJECT_ID} --key-file=${GOOGLE_APPLICATION_CREDENTIALS};
-gcsfuse -o rw,allow_other -file-mode=777 -dir-mode=777 --foreground ${BUCKET} ${MNT_DIR};
+# Create mount directory for service
+mkdir -p $MNT_DIR;
+echo "Listing directory."
+ls -l;
+pwd;
+echo "Mounting GCS Fuse.";
+gcsfuse --debug_gcs --debug_fuse $BUCKET $MNT_DIR 
+echo "Mounting completed."
+
 mkdir -p ${USER_GENERATED_DATA_DIR} && chmod 777 ${USER_GENERATED_DATA_DIR};
-cellxgene launch --host 0.0.0.0 --annotations-dir ./data/${FULL_PATH}/annotations ${GCS_FILE_LOCATION} --user-generated-data-dir ${USER_GENERATED_DATA_DIR}
+
+
+
+
+
+# Run cellxgene
+cellxgene launch --host 0.0.0.0 --annotations-dir ${USER_GENERATED_DATA_DIR}/${FULL_PATH}/annotations ${GCS_FILE_LOCATION} 
+
+wait -n
