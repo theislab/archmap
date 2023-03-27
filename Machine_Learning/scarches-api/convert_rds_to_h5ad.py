15f5c2589600a7388272f7f6dc9e141a206da0f7
xaviergeorge <13365640+xaviergeorge@users.noreply.github.com>
2023-03-27 11:57:18 +0200

    add h5ad conversion

---
+++ Machine_Learning/scarches-api/convert_rds_to_h5ad.py
@@ -0,0 +1,28 @@
+import subprocess
+import os 
+
+
+def make_anndata(local_filename):
+    """
+    Create a Anndata rds file from the Seurat file.
+    """
+    try:
+        subprocess.run(
+            [
+                "Rscript",
+                os.path.join(os.path.abspath(os.path.dirname(__file__)), "sceasy_converter_rds.R"),
+                local_filename,
+            ],
+            capture_output=True,
+            check=True,
+        )
+    except subprocess.CalledProcessError as ex:
+        msg = f"Anndata conversion failed: {ex.output} {ex.stderr}"
+        print(msg)
+        raise RuntimeError(msg) from ex
+
+    return local_filename.replace(".rds", ".h5ad")
+
+if __name__ == "__main__":
+    file_name = "test_1.rds"
+    make_anndata(file_name)
