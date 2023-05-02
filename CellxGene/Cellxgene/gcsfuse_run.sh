bb0c1d5858e2196d673d945faf6bbe12f0d365d1
xaviergeorge <13365640+xaviergeorge@users.noreply.github.com>
2023-05-02 11:52:24 +0200

    werkzeug issue

--- CellxGene/Cellxgene/gcsfuse_run.sh
+++ CellxGene/Cellxgene/gcsfuse_run.sh
@@ -10,7 +10,19 @@ echo "Mounting GCS Fuse.";
 gcsfuse --debug_gcs --debug_fuse $BUCKET $MNT_DIR 
 echo "Mounting completed."
 
+echo "Listing directory.mount "
 ls -l $MNT_DIR;
+echo "Listing directory. full path."
+ls -l $MNT_DIR/$FULL_PATH;
+echo "Listing directory annotatoins."
+ls -l $MNT_DIR/$FULL_PATH/annotations;
+
+# cd into ${MNT_DIR}/${FULL_PATH}/ 
+echo "changing directory to ${MNT_DIR}/${FULL_PATH}/"
+cd ${MNT_DIR}/${FULL_PATH}/
+
+echo "the pwd is now: "
+pwd; 
 
 # mkdir -p ${USER_GENERATED_DATA_DIR} && chmod 777 ${USER_GENERATED_DATA_DIR};
 
@@ -19,6 +31,6 @@ ls -l $MNT_DIR;
 
 
 # Run cellxgene
-cellxgene launch --host 0.0.0.0 --port 8080  --annotations-dir ${MNT_DIR}/${FULL_PATH}/annotations ${GCS_FILE_LOCATION} 
+cellxgene launch --host 0.0.0.0 --port 8080  --annotations-dir annotations  ${GCS_FILE_LOCATION} 
 
 wait -n
