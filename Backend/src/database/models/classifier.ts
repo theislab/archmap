d4bc06fcf2a90fa582567989c2a71991246dc2ad
Mohammad Moghareh Dehkordi <m.mogharehdehkordi@gmail.com>
2023-08-10 12:30:29 +0200

    impl. db model and scheme for Classifiers

---
+++ Backend/src/database/models/classifier.ts
@@ -0,0 +1,26 @@
+import { Document, model, Schema } from "mongoose";
+
+export interface ICLASSIFIER extends Document {
+  name: string;
+  description: string;
+}
+
+const classifierSchema = new Schema<ICLASSIFIER>(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+
+    description: {
+      type: String,
+      required: false,
+      default: "",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export const classifierModel = model<ICLASSIFIER>("Classifier", classifierSchema);
