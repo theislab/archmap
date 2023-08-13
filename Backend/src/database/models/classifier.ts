c6e27d1ec11a3d52b0c30eea0cc2c067108c1a9c
Mohammad Moghareh Dehkordi <m.mogharehdehkordi@gmail.com>
2023-08-13 18:37:07 +0200

    adds url for classifiers

--- Backend/src/database/models/classifier.ts
+++ Backend/src/database/models/classifier.ts
@@ -3,6 +3,7 @@ import { Document, model, Schema } from "mongoose";
 export interface ICLASSIFIER extends Document {
   name: string;
   description: string;
+  classiferUrl: string;
 }
 
 const classifierSchema = new Schema<ICLASSIFIER>(
@@ -17,6 +18,7 @@ const classifierSchema = new Schema<ICLASSIFIER>(
       required: false,
       default: "",
     },
+    classiferUrl: { type: String, required: false },
   },
   {
     timestamps: true,
