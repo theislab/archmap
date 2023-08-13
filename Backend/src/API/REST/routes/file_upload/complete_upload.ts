accb0618e1aae7d6f502029a059c0d1335f5a821
Mohammad Moghareh Dehkordi <m.mogharehdehkordi@gmail.com>
2023-08-14 00:00:24 +0200

    debugging

--- Backend/src/API/REST/routes/file_upload/complete_upload.ts
+++ Backend/src/API/REST/routes/file_upload/complete_upload.ts
@@ -66,19 +66,30 @@ export default function upload_complete_upload_route() {
         try {
           let request: S3.HeadObjectRequest = { Key: data.Key, Bucket: data.Bucket };
           console.log("request inside complete upload is ", request);
+          console.log("1 Project:");
+          console.log(project);
+          project.classifierId ? console.log(project.classifierId) : console.log("NULL ID");
           let result = await s3.headObject(request).promise();
+          console.log("2");
           const updateFileAndStatus: UpdateProjectDTO = {
             fileSize: result.ContentLength,
             status: ProjectStatus.PROCESSING_PENDING,
           };
+          console.log("3");
           await ProjectService.updateProjectByUploadId(params.UploadId, updateFileAndStatus);
+          console.log("4 Project:");
+          console.log(project);
           if (process.env.CLOUD_RUN_URL) {
             let [model, atlas, classifier] = await Promise.all([
               ModelService.getModelById(project.modelId),
               AtlasService.getAtlasById(project.atlasId),
-              project.classifierId ? ClassifierService.getClassifierById(project.classifierId) : Promise.resolve(undefined),
+              project.classifierId
+                ? ClassifierService.getClassifierById(project.classifierId)
+                : Promise.resolve(undefined),
             ]);
+            console.log("5");
             if (!model || !atlas || (!classifier && model.name !== "totalVI")) {
+              console.log("6");
               await ProjectService.updateProjectById(params.UploadId, {
                 status: ProjectStatus.PROCESSING_FAILED,
               });
@@ -88,7 +99,7 @@ export default function upload_complete_upload_route() {
                 .status(500)
                 .send(`Could not find ${!model ? "model" : !atlas ? "atlas" : "classifier"}`);
             }
-
+            console.log("7");
             //Create a token, which can be used later to update the projects status
             let { token: updateToken } = await ProjectUpdateTokenService.addToken({
               _projectId: project._id,
