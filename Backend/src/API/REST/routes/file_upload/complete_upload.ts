a3b692b082b12ca0e14830a2fb12797dced63b0f
xaviergeorge <13365640+xaviergeorge@users.noreply.github.com>
2023-11-13 11:53:25 +0100

    fix merge issue

--- Backend/src/API/REST/routes/file_upload/complete_upload.ts
+++ Backend/src/API/REST/routes/file_upload/complete_upload.ts
@@ -22,7 +22,7 @@ import { validationMdw } from "../../middleware/validation";
 import ProjectUpdateTokenService from "../../../../database/services/project_update_token.service";
 import { query_path, result_model_path, result_path } from "./bucket_filepaths";
 
-const MAX_EPOCH_QUERY = 2;
+const MAX_EPOCH_QUERY = 1;
 
 export default function upload_complete_upload_route() {
   let router = express.Router();
@@ -32,6 +32,7 @@ export default function upload_complete_upload_route() {
     check_auth(),
     async (req: ExtRequest, res) => {
       let { parts, uploadId } = req.body;
+      console.log("req.body is ", req.body);
       if (!process.env.S3_BUCKET_NAME)
         return res.status(500).send("Server was not set up correctly");
 
@@ -50,7 +51,7 @@ export default function upload_complete_upload_route() {
         let data;
         try {
           data = await s3.completeMultipartUpload(params).promise();
-          console.log("data from completeMultipart ", data)
+          console.log("data from completeMultipart ", data);
         } catch (err: any) {
           console.error(err, err.stack || "Error when completing multipart upload");
           return res.status(500).send(err);
@@ -63,45 +64,25 @@ export default function upload_complete_upload_route() {
         //Query file size and save in project
         try {
           let request: S3.HeadObjectRequest = { Key: data.Key, Bucket: data.Bucket };
+          console.log("request inside complete upload is ", request);
           let result = await s3.headObject(request).promise();
           const updateFileAndStatus: UpdateProjectDTO = {
             fileSize: result.ContentLength,
             status: ProjectStatus.PROCESSING_PENDING,
           };
-
           await ProjectService.updateProjectByUploadId(params.UploadId, updateFileAndStatus);
           if (process.env.CLOUD_RUN_URL) {
-            let model, atlas;
-
-            // Archmap core atlases
-            if(project.modelId && project.atlasId){
-              [model, atlas] = await Promise.all([
-                ModelService.getModelById(project.modelId),
-                AtlasService.getAtlasById(project.atlasId),
-              ]); 
-            }
-
-            // Check if the model and atlas exist for the chosen core archmap atlas.
-            if ((!model || !atlas) && !project.scviHubId && !project.model_setup_anndata_args) {
-              await ProjectService.updateProjectByUploadId(params.UploadId, {
+            let [model, atlas] = await Promise.all([
+              ModelService.getModelById(project.modelId),
+              AtlasService.getAtlasById(project.atlasId),
+            ]);
+            if (!model || !atlas) {
+              await ProjectService.updateProjectById(params.UploadId, {
                 status: ProjectStatus.PROCESSING_FAILED,
               });
-
-              try_delete_object_from_s3(query_path(project._id))
-              console.log("Deleteing the file from s3 with path ", query_path(project._id))
+              try_delete_object_from_s3(query_path(project._id));
+              console.log("Deleteing the file from s3 with path ", query_path(project._id));
               return res.status(500).send(`Could not find ${!model ? "model" : "atlas"}`);
-            }
-
-            // Check if the id and args exist for the chosen scvi hub atlas. 
-            if((!project.scviHubId || !project.model_setup_anndata_args) && !model && !atlas){
-              console.log(project.scviHubId && project.model_setup_anndata_args);
-              await ProjectService.updateProjectByUploadId(params.UploadId, {
-                status: ProjectStatus.PROCESSING_FAILED,
-              });
-
-              try_delete_object_from_s3(query_path(project._id))
-              console.log("Deleteing the file from s3 with path ", query_path(project._id))
-              return res.status(500).send(`Could not find ${!project.scviHubId ? "scviHubId" : "model_setup_anndata_args"}`);
             }
 
             //Create a token, which can be used later to update the projects status
@@ -110,13 +91,13 @@ export default function upload_complete_upload_route() {
             });
 
             let queryInfo;
-            if(model && model.name === "scVI" ){ // QueryInfo for the scVI model for the archmap core atlases
+            if (model.name == "scVI") {
               queryInfo = {
                 model: model.name,
                 atlas: atlas.name,
                 output_type: {
-                    csv: false,
-                    cxg: true
+                  csv: false,
+                  cxg: true,
                 },
                 query_data: query_path(project.id),
                 output_path: result_path(project.id),
@@ -129,43 +110,26 @@ export default function upload_complete_upload_route() {
                 scvi_max_epochs_query: 1, // TODO: make this a standard parameter
                 webhook: `${process.env.API_URL}/projects/updateresults/${updateToken}`,
               };
-            }else {
-              if(model && model.name === "scANVI"){ // QueryInfo for the scANVI model for the archmap core atlases
-                queryInfo = {
-                  model: model.name,
-                  atlas: atlas.name,
-                  output_type: {
-                      csv: false,
-                      cxg: true
-                  },
-                  query_data: query_path(project.id),
-                  output_path: result_path(project.id),
-                  model_path: result_model_path(project.id),
-                  reference_data: `atlas/${project.atlasId}/data.h5ad`,
-                  pre_trained_scANVI: true,
-                  ref_path: "model.pt",
-                  //ref_path: `models/${project.modelId}/model.pt`,
-                  async: false,
-                  scanvi_max_epochs_query: MAX_EPOCH_QUERY, // TODO: make this a standard parameter
-                  webhook: `${process.env.API_URL}/projects/updateresults/${updateToken}`,
-                };
-              }else{ // Query info for scvi hub atlas
-                if(project.scviHubId && project.model_setup_anndata_args){ 
-                  queryInfo = {
-                    scviHubId: project.scviHubId,
-                    model_setup_anndata_args: project.model_setup_anndata_args,
-                    output_type: {
-                        csv: false,
-                        cxg: true
-                    },
-                    query_data: query_path(project.id),
-                    output_path: result_path(project.id),
-                    async: false,
-                    webhook: `${process.env.API_URL}/projects/updateresults/${updateToken}`,
-                  };
-                }
+            } else {
+              queryInfo = {
+                model: model.name,
+                atlas: atlas.name,
+                output_type: {
+                  csv: false,
+                  cxg: true,
+                },
+                query_data: query_path(project.id),
+                output_path: result_path(project.id),
+                model_path: result_model_path(project.id),
+                reference_data: `atlas/${project.atlasId}/data.h5ad`,
+                pre_trained_scANVI: true,
+                ref_path: "model.pt",
+                //ref_path: `models/${project.modelId}/model.pt`,
+                async: false,
+                scanvi_max_epochs_query: MAX_EPOCH_QUERY, // TODO: make this a standard parameter
+                webhook: `${process.env.API_URL}/projects/updateresults/${updateToken}`,
+              };
             }
-
             console.log("sending: ");
             console.log(queryInfo);
             const url = `${process.env.CLOUD_RUN_URL}/query`;
@@ -176,6 +140,7 @@ export default function upload_complete_upload_route() {
             //Processing is synchronous, response is sent by ML only after the result is produced, might take some time
             let result;
 
+            // call liveness for debugging
             try {
               // this leads to ECONNRESET
               const liveness_url = `${process.env.CLOUD_RUN_URL}/liveness`;
@@ -258,7 +223,7 @@ export default function upload_complete_upload_route() {
               let content: Buffer = await new Promise((resolve, reject) => {
                 fs.readFile(
                   path.join(__dirname, "../../../../../dev/test_file1.csv"),
-                  function(err, data) {
+                  function (err, data) {
                     if (err) reject(err);
                     else resolve(data);
                   }
@@ -290,11 +255,10 @@ export default function upload_complete_upload_route() {
             }
           } else {
             const updateStatus: UpdateProjectDTO = { status: ProjectStatus.PROCESSING_FAILED };
-            console.log("Status updated to failed")
             await ProjectService.updateProjectByUploadId(params.UploadId, updateStatus);
-            console.log("Status updated to failed")
+            console.log("Status updated to failed");
             try_delete_object_from_s3(query_path(project._id));
-            console.log("Deleting the file from s3 with path ", query_path(project._id) )
+            console.log("Deleting the file from s3 with path ", query_path(project._id));
             return res.status(500).send("Processing failed!");
           }
         } catch (err) {
