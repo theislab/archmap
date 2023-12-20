ce0bd21a8ce36eeb57795e1521648e34a3c7fed5
ronskoro <rskorobogat@gmail.com>
2023-12-20 18:16:12 +0100

    fix demo issue on team page

--- Frontend/src/components/teams/detail/TeamProjectList/index.jsx
+++ Frontend/src/components/teams/detail/TeamProjectList/index.jsx
@@ -6,11 +6,26 @@ import {
   DialogActions, DialogContent, DialogContentText,
 } from '@mui/material';
 import ProjectService from 'shared/services/Project.service';
+import DemoService from 'shared/services/Demo.service';
 import ProjectBarCard from 'components/GeneMapper/projectBarCard';
 import AtlasService from 'shared/services/Atlas.service';
 import ModelService from 'shared/services/Model.service';
 import TeamService from 'shared/services/Team.service';
+import { PROJECT_STATUS } from 'shared/utils/common/constants';
 
+/**
+ * TeamProjectList Component:
+ * This component renders a list of projects associated with a specific team. It manages the display
+ * of projects, allows administrators to remove projects, handles modal dialogs for project removal,
+ * and fetches necessary data from various services.
+ *
+ * @param {Object} team - The team object for which projects are being listed.
+ * @param {Object} institution - The institution associated with the team.
+ * @param {Object} user - The current user object.
+ * @param {boolean} isAdmin - Flag indicating if the current user is an admin.
+ * @param {Function} updateTeam - Function to update team information after project removal.
+ * @returns {JSX.Element} - A JSX element displaying the list of projects for the team.
+ */
 function TeamProjectList({
   team, institution, user, isAdmin, updateTeam,
 }) {
@@ -24,6 +39,8 @@ function TeamProjectList({
   const [snackbarOpen, setSnackbarOpen] = useState(false);
   const [removedProject, setRemovedProject] = useState([]);
   const [errorMessage, setErrorMessage] = useState('');
+
+  const [demos, setDemos] = useState([]);
 
   const handleCloseDialog = () => setDialogOpen(false);
 
@@ -54,12 +71,45 @@ function TeamProjectList({
     setSnackbarOpen(false);
   };
 
+  /**
+  * Function to find matching demos in the data variable.
+  * If demos are found, the information stored about them in the "data" variable is updated.
+  * @param data - The data variable containing the projects to search for demos in.
+  * @param demos - The array of available demos. 
+  */
+    const findDemos = (data, demos) => {
+      // loop over projects and search for demo datasets and set their attributes
+      for (let i = 0; i < data.length; i++) {
+        for (let j = 0; j < demos.length; j++) {
+          // If the project is a demo, the demo id is stored in the file name.
+          // file_name: <atlas>_<model>_<demo_id>
+          const id = data[i].fileName.split('_')[2];
+          // update the stored information about the demo datasets
+          if (id && id === demos[j]._id) {
+            // updating the demo dataset
+            data[i] = {
+              ...data[i],
+              status: PROJECT_STATUS.DONE,
+              location: demos[j].csvURL,
+            };
+          }
+        }
+      }
+    };
+
   useEffect(() => {
     setIsLoading(true);
     AtlasService.getAtlases().then((data) => setAtlases(data));
     ModelService.getModels().then((data) => setModels(data));
     TeamService.getMyTeams().then((teams) => setUserTeams(teams));
-    ProjectService.getTeamProjects(team._id).then((data) => setProjects(data));
+    DemoService.getDemos().then((demos) => {
+      setDemos(demos);
+      // fetch the projects and update the information about the demos
+      ProjectService.getTeamProjects(team._id).then((data) => {
+        findDemos(data, demos);
+        setProjects(data);
+      });
+    }); 
     setIsLoading(false);
   }, [team]);
 
