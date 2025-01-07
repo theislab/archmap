4b04c9d929ff662bc039d9aacf53433eb74e8e61
ChelseaBright96 <chels.alex.bright@gmail.com>
2025-01-07 11:27:02 +0100

    add logging

--- Backend/src/API/REST/routes/atlas/atlasRouter.ts
+++ Backend/src/API/REST/routes/atlas/atlasRouter.ts
@@ -180,15 +180,16 @@ const trigger_cloud_run_job = (): Router => {
   router.post("/trigger-job", async (req, res) => {
     try {
       const endpoint = "https://europe-west3-custom-helix-329116.cloudfunctions.net/trigger-job";
+      console.log("endpoint", endpoint)
 
       // Prepare the request headers
-      const headers = {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${process.env.ACCESS_TOKEN}`, // Ensure valid authentication if required
-      };
+      // const headers = {
+      //   "Content-Type": "application/json",
+      //   Authorization: `Bearer ${process.env.ACCESS_TOKEN}`, // Ensure valid authentication if required
+      // };
 
       // Send the POST request
-      const response = await axios.post(endpoint, {}, { headers });
+      const response = await axios.post(endpoint, {});
 
       // Respond to the client with the result
       res.status(200).json({
