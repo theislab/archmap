5065a8720a6b177205dc5ede28da94a3ffe63f74
Chelsea Bright <chels.alex.bright@gmail.com>
2025-11-15 11:17:50 +0100

    fix bug

--- Backend/src/API/REST/routes/atlas/atlasRouter.ts
+++ Backend/src/API/REST/routes/atlas/atlasRouter.ts
@@ -226,7 +226,7 @@ function generatePublicAtlasToken(): string {
 const get_allAtlases = (): Router => {
   const router = express.Router();
 
-  router.get("/atlases", optional_auth(), async (req: any, res) => {
+  router.get("/atlases", check_auth(), async (req: any, res) => {
     try {
       // If no JWT provided, auto-generate public one
       let public_jwt: string | null = null;
